e9c412f3989fbdda1036ff81a5d7718682e6a55f
Merlin033 <barnabedebocq@live.fr>
2023-05-01 00:09:13 +0200

    BACKGROUND FOREST / v1.0

--- main.js
+++ main.js
@@ -8,14 +8,15 @@ import { UI } from "./UI.js";
 window.addEventListener("load", () => {
 	const canvas = document.getElementById("canvas1");
 	const ctx = canvas.getContext("2d");
-	canvas.width = 500;
+	canvas.width = 900;
 	canvas.height = 500;
 
 	class Game {
 		constructor(width, height) {
 			this.width = width;
 			this.height = height;
-			this.groundMargin = 80;
+			// cityGround this.groundMargin = 80;
+			this.groundMargin = 40;
 			this.speed = 0;
 			this.maxSpeed = 3;
 			this.background = new Background(this);
@@ -23,13 +24,26 @@ window.addEventListener("load", () => {
 			this.input = new InputHandler(this);
 			this.UI = new UI(this);
 			this.enemies = [];
+			this.particles = [];
+			this.collisions = [];
+			this.floatingMessages = [];
+			this.maxParticles = 50;
 			this.enemyTimer = 0;
 			this.enemyInterval = 1000;
-			this.debug = true;
+			this.debug = false;
 			this.score = 0;
+			this.winningScore = 40;
 			this.fontColor = "black";
+			this.time = 0;
+			this.maxTime = 30000;
+			this.gameOver = false;
+			this.lives = 5;
+			this.player.currentState = this.player.states[0];
+			this.player.currentState.enter();
 		}
 		update(deltaTime) {
+			this.time += deltaTime;
+			if (this.time > this.maxTime) this.gameOver = true;
 			this.background.update();
 			this.player.update(this.input.keys, deltaTime);
 			//handleEnemies
@@ -41,14 +55,41 @@ window.addEventListener("load", () => {
 			}
 			this.enemies.forEach((enemy) => {
 				enemy.update(deltaTime);
-				if (enemy.markedForDeletion) this.enemies.splice(this.enemies.indexOf(enemy), 1);
 			});
+			//handle messages
+			this.floatingMessages.forEach((message) => {
+				message.update();
+			});
+			//handle particles
+			this.particles.forEach((particle, index) => {
+				particle.update();
+			});
+			if (this.particles.length > this.maxParticles) {
+				this.particles.length = this.maxParticles;
+			}
+			//handle collision sprites
+			this.collisions.forEach((collision, index) => {
+				collision.update(deltaTime);
+			});
+			this.enemies = this.enemies.filter((enemy) => !enemy.markedForDeletion);
+			this.particles = this.particles.filter((particle) => !particle.markedForDeletion);
+			this.collisions = this.collisions.filter((collision) => !collision.markedForDeletion);
+			this.floatingMessages = this.floatingMessages.filter((message) => !message.markedForDeletion);
 		}
 		draw(context) {
 			this.background.draw(context);
 			this.player.draw(context);
 			this.enemies.forEach((enemy) => {
 				enemy.draw(context);
+			});
+			this.particles.forEach((particle) => {
+				particle.draw(context);
+			});
+			this.collisions.forEach((collision) => {
+				collision.draw(context);
+			});
+			this.floatingMessages.forEach((message) => {
+				message.draw(context);
 			});
 			this.UI.draw(context);
 		}
@@ -70,7 +111,7 @@ window.addEventListener("load", () => {
 		ctx.clearRect(0, 0, canvas.width, canvas.height);
 		game.update(deltaTime);
 		game.draw(ctx);
-		requestAnimationFrame(animate);
+		if (!game.gameOver) requestAnimationFrame(animate);
 	}
 	animate(0);
 });
